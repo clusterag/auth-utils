7b974ce9ea585878017fb255dd85f805907bb30e
John Eberle <john@clusterag.leiningergymnasium.de>
2014-06-20 12:44:38 +0200

    changed indentation to tabs, etc

--- accnamemaker/accnamemaker.py
+++ accnamemaker/accnamemaker.py
@@ -2,50 +2,48 @@ import datetime
 
 
 def date_to_number(date):
-	"""take the 8-10 character long birthdate as a string and convert it to the three magic digits."""
-	pos_of_dot = date.index('.')
-	if date.count('.') <> 2:
-		return
-	else:
-		if len(date) == 10:  #for 01.01.2014
-			days = int(date[0:2])
-			months = int(date[3:5])
-			years = int(date[6:10])
-		elif len(date) == 9:
-			if pos_of_dot == 2:  #for 01.1.2014
-				days = int(date[0:2])
-				months = int(date[3])
-				years = int(date[5:9])
-			elif pos_of_dot == 1:  #for 1.01.2014
-				days = int(date[0])
-				months = int(date[3:4])
-				years = int(date[5:9])
-			else:
-				return
-		elif len(date) == 8:  #for 1.1.2014
-				days = int(date[0])
-				months = int(date[2])
-				years = int(date[4:8])
-		else:
-			return
-	diff = datetime.date(years, months, days)-datetime.date(1899, 12, 30)
-	diff = diff.days
-	diff = str(diff)
-	number = diff[2:5]
-	return number
+    """take the 8-10 character long birthdate as a string and convert it to the three magic digits."""
+    pos_of_dot = date.index('.')
+    if date.count('.') != 2:
+        return
+    else:
+        if len(date) == 10:  # for 01.01.2014
+            days = int(date[0:2])
+            months = int(date[3:5])
+            years = int(date[6:10])
+        elif len(date) == 9:
+            if pos_of_dot == 2:  # for 01.1.2014
+                days = int(date[0:2])
+                months = int(date[3])
+                years = int(date[5:9])
+            elif pos_of_dot == 1:  # for 1.01.2014
+                days = int(date[0])
+                months = int(date[3:4])
+                years = int(date[5:9])
+            else:
+                return
+        elif len(date) == 8:  # for 1.1.2014
+                days = int(date[0])
+                months = int(date[2])
+                years = int(date[4:8])
+        else:
+            return
+    diff = datetime.date(years, months, days)-datetime.date(1899, 12, 30)
+    diff = diff.days
+    diff = str(diff)
+    number = diff[2:5]
+    return number
 
 
 def name_to_shortname(name):
-	"""take the first two letters from the first and from the last name."""
-	name = name.lower()
-	if name.count(' ') == 1:
-		pos_of_space = name.index(' ')
-		part1 = name[0:2]
-		part2 = name[pos_of_space+1:pos_of_space+3]
-		shortname = part1+part2
-		return shortname
+    """take the first two letters from the first and from the last name."""
+    name = name.lower()
+    if name.count(' ') == 1:
+        pos_of_space = name.index(' ')
+        part1 = name[0:2]
+        part2 = name[pos_of_space+1:pos_of_space+3]
+        shortname = part1+part2
+        return shortname
 
 
 print name_to_shortname("Max Mustermann")+date_to_number("1.1.1970")
-
-
--- pwmaker/pwmaker.py
+++ pwmaker/pwmaker.py
@@ -17,14 +17,15 @@ user_file.close()
 
 users_and_pws = []
 
-"""for user in users:
+for user in users:
     user = user[:-1]
     pw = ""
     for i in range(7):
         pw += random.choice(letters)
     # pw = user + ";" + pw + "\n"
     append_to_file("users.csv", user + ";" + pw + "\n")
-    append_to_file("users_hashed.csv", user + ";" + str(bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()))[2:-1] + "\n")"""
+    append_to_file("users_hashed.csv", user + ";" + str(
+        bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()))[2:-1] + "\n")
 pw = ""
 for i in range(7):
     pw += random.choice(letters)

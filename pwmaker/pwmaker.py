ce0c434010326449c9e79974e632df05e8a686a3
John Eberle <john@clusterag.leiningergymnasium.de>
2014-06-20 13:15:25 +0200

    moving pwmaker to new CLI interface

--- pwmaker/pwmaker.py
+++ pwmaker/pwmaker.py
@@ -2,6 +2,7 @@
 import random
 import bcrypt
 from string import hexdigits as letters
+from sys import argv as arguments
 
 
 def append_to_file(filepath, string):
@@ -11,22 +12,42 @@ def append_to_file(filepath, string):
     outfile.close
 
 
-user_file = open("users")
-users = user_file.readlines()
-user_file.close()
-
-users_and_pws = []
-
-for user in users:
-    user = user[:-1]
+def gen_pw():
+    """generate a password."""
     pw = ""
     for i in range(7):
         pw += random.choice(letters)
-    # pw = user + ";" + pw + "\n"
-    append_to_file("users.csv", user + ";" + pw + "\n")
-    append_to_file("users_hashed.csv", user + ";" + str(
-        bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()))[2:-1] + "\n")
-pw = ""
-for i in range(7):
-    pw += random.choice(letters)
-print(pw)
+    hashed = str(
+        bcrypt.hashpw(pw.encode("utf-8"), bcrypt.gensalt()))[2:-1]
+    return pw, hashed
+
+
+def get_users(user_file_path):
+    """read usernames from file."""
+    user_file = open(user_file_path)
+    users = user_file.readlines()
+    user_file.close()
+
+
+def single_password():
+    """print a single password and exit."""
+    print(gen_pw)
+
+
+def batch():
+    """generate to csv."""
+    user_file_path = input("path to file containing user names:")
+    plaintext_file_path = input("output path of csv file containing passwords in plaintext:")
+    hashed_file_path = input("output path of csv file containing hashed passwords:")
+    users_and_pws = []
+    users = get_users(user_file_path)
+    for user in users:
+        user = user[:-1]
+        pw, hashed = gen_pw()
+        append_to_file(plaintext_file_path, user + ";" + pw + "\n")
+        append_to_file(hashed_file_path, user + ";" + hashed + "\n")
+
+if arguments[1] == "batch":
+    batch()
+else:
+    single_password()
